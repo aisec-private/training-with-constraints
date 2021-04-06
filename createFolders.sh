a22e97ef1925ed1a0ab081f77817947786eb5d61
Tgl70 <bruceketta70@yahoo.it>
2021-04-06 12:13:58 +0200

    initial commit

---
+++ createFolders.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir datasets
+mkdir datasets\fashion_mnist
+mkdir datasets\fashion_mnist\baseline
+mkdir datasets\fashion_mnist\augmented
+mkdir datasets\fashion_mnist\augmented_FGSM
+mkdir datasets\gtsrb
+mkdir datasets\gtsrb\baseline
+mkdir datasets\gtsrb\augmented
+mkdir datasets\gtsrb\augmented_FGSM
+
+mkdir models
+mkdir models\fashion_mnist
+mkdir models\fashion_mnist\baseline
+mkdir models\fashion_mnist\augmented
+mkdir models\fashion_mnist\augmented_FGSM
+mkdir models\fashion_mnist\dl2
+mkdir models\gtsrb
+mkdir models\gtsrb\baseline
+mkdir models\gtsrb\augmented
+mkdir models\gtsrb\augmented_FGSM
+mkdir models\gtsrb\dl2
